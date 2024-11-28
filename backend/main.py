494326a695c7b633b48710b56c7f6f0fd8a66c31
Vachan <vachanshetty95@gmail.com>
2024-11-28 21:54:59 +0100

    feat: Add routers and prepopulate data at startup

--- backend/main.py
+++ backend/main.py
@@ -1,15 +1,26 @@
-import os
 import logging
+import os
 from contextlib import asynccontextmanager
 
 from fastapi import FastAPI
 from fastapi.routing import APIRoute
+from models.database import engine, init_db
+from routes.routers import api_router
+from sqlmodel import Session
+
+from backend.models.seeding import prepopulate_data
 
 
 @asynccontextmanager
 async def lifespan(app: FastAPI):
     logging.info("Application has started...")
+    init_db()
+    logging.info("Database startup completed")
+    with Session(engine) as session:
+        logging.info("Loading dummy data")
+        prepopulate_data(session=session)
     yield
+
 
 def custom_generate_unique_id(route: APIRoute) -> str:
     return f"{route.tags[0]}-{route.name}"
@@ -23,3 +34,5 @@ app = FastAPI(
     generate_unique_id_function=custom_generate_unique_id,
     lifespan=lifespan,
 )
+
+app.include_router(api_router, prefix=API_V1_STR)
